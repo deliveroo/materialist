f3668f40cb7baf9d36de42d7340a579c84798020
Jakub Novotny <novotny.jakub@gmail.com>
2018-04-02 12:29:17 +0100

    Use materializer sidekiq options in EventHandler
    This means we can use the provided EventHandler in projects where we
    want to specify different sidekiq queues and other options. We won't
    have to copy and paste a different one.

--- spec/materialist/event_handler_spec.rb
+++ spec/materialist/event_handler_spec.rb
@@ -3,18 +3,21 @@ require 'materialist/event_handler'
 require 'materialist/event_worker'
 
 RSpec.describe Materialist::EventHandler do
+  let(:configuration) do
+    OpenStruct.new({
+      sidekiq_options: sidekiq_options,
+      topics: topics
+    })
+  end
   let(:topics) {[]}
   let(:sidekiq_options) {{}}
+  let(:materializer_class) { double(_sidekiq_options: {}) }
   let(:worker_double) { double() }
 
   before do
-    Materialist.configure do |config|
-      config.sidekiq_options = sidekiq_options
-      config.topics = topics
-    end
-
-    allow(Materialist::EventWorker).to receive(:set)
-      .and_return worker_double
+    allow(Materialist).to receive(:configuration).and_return(configuration)
+    allow(Materialist::MaterializerFactory).to receive(:class_from_topic).and_return(materializer_class)
+    allow(Materialist::EventWorker).to receive(:set).and_return(worker_double)
   end
 
   describe "#on_events_received" do
@@ -50,56 +53,30 @@ RSpec.describe Materialist::EventHandler do
   end
 
   describe "#call" do
-    subject { described_class.new.call }
+    subject { described_class.new.call(event) }
 
-    let(:event) { { "topic" => topic } }
-    let(:topic) { :foobar }
     let(:configuration) do
       OpenStruct.new({
-        sidekiq_options: sidekiq_options
+        sidekiq_options: { unique: false, retry: 5 }
       })
     end
-    let(:sidekiq_options) { { "option_1" => "value" } }
+    let(:event) { { "topic" => topic, "data" => "data" } }
+    let(:topic) { "foobar" }
+    let(:materializer_class) { double(_sidekiq_options: materializer_sidekiq_options) }
+    let(:materializer_sidekiq_options) { { queue: :dedicated, retry: 2 } }
+    let(:expected_event_options) { { queue: :dedicated, retry: 2, unique: false } }
+    let(:worker_class) { double(perform_async: nil) }
 
     before do
       allow(Materialist).to receive(:configuration).and_return(configuration)
       allow(Materialist::MaterializerFactory).to receive(:class_from_topic).with(topic).and_return(materializer_class)
+      allow(Materialist::EventWorker).to receive(:set).with(expected_event_options).and_return(worker_class)
     end
 
-    context "when the materializer class specifies a queue"
-    context "when the materializer class does not specify a queue"
-  end
+    it "enqueues the event worker with sidekiq options merged from configuration, default and the materializer" do
+      subject
 
-  describe "#call" do
-    let(:event) { { "topic" => :foobar } }
-    let(:perform) { subject.call event }
-
-    it "enqueues the event" do
-      expect(worker_double).to receive(:perform_async).with(event)
-      perform
-    end
-
-    context "if queue name is privided" do
-      let(:queue_name) { :some_queue_name }
-      let(:sidekiq_options) {{ queue: queue_name }}
-
-      it "enqueues the event in the given queue" do
-        expect(Materialist::EventWorker).to receive(:set)
-          .with(queue: queue_name, retry: 10)
-        expect(worker_double).to receive(:perform_async).with(event)
-        perform
-      end
-    end
-
-    context "when a retry is specified in options" do
-      let(:sidekiq_options) {{ retry: false }}
-
-      it "uses the given retry option for sidekiq" do
-        expect(Materialist::EventWorker).to receive(:set)
-          .with(retry: false)
-        expect(worker_double).to receive(:perform_async).with(event)
-        perform
-      end
+      expect(worker_class).to have_received(:perform_async).with(event)
     end
   end
 end
